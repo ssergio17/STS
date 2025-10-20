fa9050da00eb1ab9be9787095854a74ce52708e2
ssergio17 <sergiofreirecuenca17@gmail.com>
2025-10-21 00:09:37 +0200

    Cambios

--- FormularioAgenciaEspacial/src/main/java/com/example/demo/FormularioControlador.java
+++ FormularioAgenciaEspacial/src/main/java/com/example/demo/FormularioControlador.java
@@ -11,25 +11,38 @@ public class FormularioControlador {
 	static Agente agente = new Agente();
 	
 	@GetMapping("/nombre")
-	public String Inicio(Agente agente, Model modelo) {
+	public String Inicio(@ModelAttribute Agente formAgente, Model modelo) {
 		modelo.addAttribute("agente", agente);
 		return "nombre";
 	}
 	
 	@PostMapping("/nombre")
-	public String FormularioNombre(Agente agente, Model modelo) {
+	public String FormularioNombre(@ModelAttribute Agente formAgente) {
+		agente.setNombre(formAgente.getNombre());
+		return "nacionalidad";
+	}
+	
+	@GetMapping("/nacionalidad")
+	public String InicioNacionalidad(@ModelAttribute Model modelo) {
 		modelo.addAttribute("agente", agente);
 		return "nacionalidad";
 	}
 	
 	@PostMapping("/nacionalidad")
-	public String FormularioNacionalidad(Agente agente, Model modelo) {
+	public String FormularioNacionalidad(@ModelAttribute Agente formAgente) {
+		agente.setNacionalidad(formAgente.getNacionalidad());
+		return "planetas";
+	}
+	
+	@GetMapping("/planetas")
+	public String InicioPlanetas(@ModelAttribute Model modelo) {
 		modelo.addAttribute("agente", agente);
 		return "planetas";
 	}
 	
 	@PostMapping("/planetas")
-	public String FormularioPlanetas(Agente agente, Model modelo) {
+	public String FormularioPlanetas(@ModelAttribute Agente formAgente, Model modelo) {
+		agente.setPlanetas(formAgente.getPlanetas());
 		modelo.addAttribute("agente", agente);
 		return "resultado";
 	}
