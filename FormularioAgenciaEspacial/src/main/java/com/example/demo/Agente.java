80c64d6c2a5bb524062e512838b5663902073dd9
ssergio17 <sergiofreirecuenca17@gmail.com>
2025-10-20 23:19:28 +0200

    Cambios

--- FormularioAgenciaEspacial/src/main/java/com/example/demo/Agente.java
+++ FormularioAgenciaEspacial/src/main/java/com/example/demo/Agente.java
@@ -1,5 +1,37 @@
 package com.example.demo;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Agente {
-	private String nombre, planeta;
+	private String nombre, nacionalidad;
+	private List<String> planetas = new ArrayList<>();
+	
+	public Agente() {}
+	
+	// Getters
+	public String getNombre() {
+		return nombre;
+	}
+	public String getNacionalidad() {
+		return nacionalidad;
+	}
+	public List<String> getPlanetas() {
+		return planetas;
+	}
+	
+	// Setters
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public void setNacionalidad(String nacionalidad) {
+		this.nacionalidad = nacionalidad;
+	}
+	public void setPlanetas(List<String> planetas) {
+		this.planetas = planetas;
+	}
+
+	@Override
+	public String toString() {
+		return "Agente [nombre=" + nombre + ", nacionalidad=" + nacionalidad + ", planetas=" + planetas + "]";
+	}
 }
--- FormularioAgenciaEspacial/src/main/java/com/example/demo/FormularioControlador.java
+++ FormularioAgenciaEspacial/src/main/java/com/example/demo/FormularioControlador.java
@@ -1,5 +1,36 @@
 package com.example.demo;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
+@Controller
 public class FormularioControlador {
-
+	
+	static Agente agente = new Agente();
+	
+	@GetMapping("/nombre")
+	public String Inicio(Agente agente, Model modelo) {
+		modelo.addAttribute("agente", agente);
+		return "nombre";
+	}
+	
+	@PostMapping("/nombre")
+	public String FormularioNombre(Agente agente, Model modelo) {
+		modelo.addAttribute("agente", agente);
+		return "nacionalidad";
+	}
+	
+	@PostMapping("/nacionalidad")
+	public String FormularioNacionalidad(Agente agente, Model modelo) {
+		modelo.addAttribute("agente", agente);
+		return "planetas";
+	}
+	
+	@PostMapping("/planetas")
+	public String FormularioPlanetas(Agente agente, Model modelo) {
+		modelo.addAttribute("agente", agente);
+		return "resultado";
+	}
 }
