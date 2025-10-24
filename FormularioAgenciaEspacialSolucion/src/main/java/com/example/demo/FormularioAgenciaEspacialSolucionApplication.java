3c940749cd0dda21d344d8fa7ecc721fe5c8458e
ssergio17 <sergiofreirecuenca17@gmail.com>
2025-10-24 14:17:29 +0200

    Solución - Terminar

---
+++ FormularioAgenciaEspacialSolucion/src/main/java/com/example/demo/FormularioAgenciaEspacialSolucionApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormularioAgenciaEspacialSolucionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormularioAgenciaEspacialSolucionApplication.class, args);
+	}
+
+}
