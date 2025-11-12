526067b4e8e4610e33eca88f017d5efdfcafc633
ssergio17 <sergiofreirecuenca17@gmail.com>
2025-11-12 09:42:01 +0100

    Empezado D3-UD3-CarritoCompra

---
+++ D3-UD3-CarritoCompra/src/main/java/com/example/demo/D3Ud3CarritoCompraApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class D3Ud3CarritoCompraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(D3Ud3CarritoCompraApplication.class, args);
+	}
+
+}
