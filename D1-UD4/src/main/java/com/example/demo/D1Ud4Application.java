c2a90c9eb5f632e9c2b8d97745fdd1b8531f0968
ssergio17 <sergiofreirecuenca17@gmail.com>
2025-12-16 10:14:13 +0100

    Cambios

---
+++ D1-UD4/src/main/java/com/example/demo/D1Ud4Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class D1Ud4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(D1Ud4Application.class, args);
+	}
+
+}
--- FormularioAgenciaEspacial/src/main/java/com/example/demo/FormularioControlador.java
+++ FormularioAgenciaEspacial/src/main/java/com/example/demo/FormularioControlador.java
@@ -5,7 +5,6 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-
 import jakarta.validation.Valid;
 
 @Controller
