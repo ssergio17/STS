f6ed3f8c50420acb14493d3e07802e6f15042cb1
ssergio17 <sergiofreirecuenca17@gmail.com>
2026-01-12 10:22:01 +0100

    Cambios

--- D1-UD4/src/main/java/com/example/demo/Quiz.java
+++ D1-UD4/src/main/java/com/example/demo/Quiz.java
@@ -1,6 +1,5 @@
 package com.example.demo;
 import java.time.LocalDateTime;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
--- D1-UD4/src/main/java/com/example/demo/QuizController.java
+++ D1-UD4/src/main/java/com/example/demo/QuizController.java
@@ -1,13 +1,11 @@
 package com.example.demo;
 import java.time.LocalDateTime;
 import java.util.List;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
 import jakarta.servlet.http.HttpSession;
 
 @Controller
